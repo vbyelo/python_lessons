14e242caa3f8c1e030873f6aa9484c62802e44a0
vbyelo <56196047+vbyelo@users.noreply.github.com>
2019-11-09 15:58:34 +0100

    Update complex.py

--- complex.py
+++ complex.py
@@ -1,20 +1,18 @@
 class Complex:
-
     def __init__(self, real, imag):
         self.real = real
         self.imag = imag
 
     def __str__(self):
-        return f'{self.real} + {self.imag}i'
+        return f"{self.real} + {self.imag}i"
 
     def __add__(self, other):
-        return Complex(real=self.real+other.real,
-         imag=self.imag+other.imag)
+        return Complex(real=self.real + other.real, imag=self.imag + other.imag)
 
 
-if __name__ == '__main__':
-    c1 = Complex(1,2)
-    c2 = Complex(3,4)
+if __name__ == "__main__":
+    c1 = Complex(1, 2)
+    c2 = Complex(3, 4)
     print(c1)
     print(c2)
     c3 = c1 + c2
