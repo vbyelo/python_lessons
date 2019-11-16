8a7c236267f5ce71c2e8c5b911d8318379ebe478
vbyelo <56196047+vbyelo@users.noreply.github.com>
2019-11-16 10:43:15 +0100

    Update quadratic_equation.py

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,14 @@
 # https://www.testdome.com/d/python-interview-questions/9
 
 import math
-def find_roots(a,b,c):
+def find_roots(*args):
+    print('typ parametrów', type(args), 'wartosci', args)
+     
+    a = args[0]
+    b = args[1]
+    c = args[2]
+    
     determinant =b**2 -4*a*c
-    
-    
-    #print('determinant',determinant)
     if determinant >0:
         x1 = (-b + math.sqrt(determinant))/(2*a)
         x2 = (-b - math.sqrt(determinant))/(2*a)
@@ -14,5 +17,7 @@ def find_roots(a,b,c):
         x1 = -b/(2*a)
         return x1
     else:
-        print('nie ma rozwiązań')
-print(find_roots(1,2,1))
+        return 'nie ma rozwiązań'
+
+
+print(find_roots(1,2,1,3))
