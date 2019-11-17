ad6618f6e61e03fa8d9a0fbbc64cb3fdb509c539
vbyelo <56196047+vbyelo@users.noreply.github.com>
2019-11-17 15:43:40 +0100

    Update conflict_map.py

--- map/conflict_map.py
+++ map/conflict_map.py
@@ -7,6 +7,9 @@ if 0 in df.index:
     df.drop([0], inplace=True)
 df.info()
 
+print(df.head())
+print(df['event_type'].unique())
+
 
 df["latitude"] = df["latitude"].astype(str).astype(float)
 df["longitude"] = df["longitude"].astype(str).astype(float)
@@ -15,11 +18,21 @@ longitude_mean = df["longitude"].astype(float).mean()
 map = folium.Map([latitude_mean, longitude_mean])
 
 for i in range(len(df)):
+    color = 'red'
+
+    if df.iloc[i]['event_type'] == 'Protests':
+        color = 'blue'
+    elif df.iloc[i]['event_type'] == 'Riots':
+        color = 'yellow'
+    elif df.iloc[i]['event_type'] == 'Battles':
+        color = 'green'
+
 
     folium.CircleMarker(
         [df.iloc[i]["latitude"], df.iloc[i]["longitude"]],
         radius=5 + int(df.iloc[i]["fatalities"]),
         fill=True,
+        color=color
     ).add_to(map)
 
 
