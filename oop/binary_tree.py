cb3cf913f54d7d817caf6e606e2639ff8c00e07e
vbyelo <56196047+vbyelo@users.noreply.github.com>
2019-11-17 13:18:33 +0100

    Create binary_tree.py

---
+++ oop/binary_tree.py
@@ -0,0 +1,19 @@
+import collections
+
+class Node:
+
+    def __init__(self, value, right, left):
+        self.value = value
+        self.right = right
+        self.left = left
+
+# Node = collections.namedtuple('Node', ['left', 'right', 'value'])
+
+def contains(root, value):
+    pass
+
+n1 = Node(value=1, left=None, right=None)
+n3 = Node(value=3, left=None, right=None)
+n2 = Node(value=2, left=n1, right=n3)
+        
+print(contains(n2, 1))
