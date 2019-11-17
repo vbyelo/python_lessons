705a6e30cea08afd1c25537ceb37fb5ebe00f6e3
vbyelo <56196047+vbyelo@users.noreply.github.com>
2019-11-17 13:34:45 +0100

    Update binary_tree.py

--- oop/binary_tree.py
+++ oop/binary_tree.py
@@ -16,8 +16,17 @@ class Node:
 def contains(root, value):
     pass
 
+
 n1 = Node(value=1, left=None, right=None)
-n3 = Node(value=3, left=None, right=None)
-n2 = Node(value=2, left=n1, right=n3)
+n4 = Node(value=4, left=None, right=None)
+n7 = Node(value=7, left=None, right=None)
+n13 = Node(value=13, left=None, right=None)
+n14 = Node(value=14, left=n13, right=None)
+n6 = Node(value=6, left=n4, right=n7)
+n3 = Node(value=3, left=n1, right=n6)
+n10 = Node(value=10, left=None, right=n14)
+n8 = Node(value=8, left=n13, right=n10)
+        
+print(contains(n8, 1))
         
 print(contains(n2, 1))
