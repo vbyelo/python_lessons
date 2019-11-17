027ada4911871ec1ecc33eb885416780ec8dead2
vbyelo <56196047+vbyelo@users.noreply.github.com>
2019-11-17 11:16:09 +0100

    Create conflict_map.py

---
+++ map/conflict_map.py
@@ -0,0 +1,26 @@
+import csv
+import pandas as pd
+import folium
+
+df = pd.read_csv("Conflict Data for Greece.csv", header=0)
+if 0 in df.index:
+    df.drop([0], inplace=True)
+df.info()
+print(df.iloc[0,:])
+print(df.iloc[1,:])
+
+df['latitude'] = df['latitude'].astype(str).astype(float)
+df['longitude'] = df['longitude'].astype(str).astype(float)
+latitude_mean = df['latitude'].astype(float).mean()
+longitude_mean = df['longitude'].astype(float).mean()
+map = folium.Map([latitude_mean,longitude_mean])
+
+for i in range(len(df)):
+    
+    folium.CircleMarker([df.iloc[i]['latitude'], df.iloc[i]['longitude']],
+                        radius=5 + int(df.iloc[i]['fatalities']),
+                        fill=True,
+                        ).add_to(map)
+
+
+map.save(outfile='map1.html')
